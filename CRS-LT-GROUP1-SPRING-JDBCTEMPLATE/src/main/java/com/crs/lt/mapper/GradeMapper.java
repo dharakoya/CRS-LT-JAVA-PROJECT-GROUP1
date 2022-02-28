4ffc25e733fc1da702591e851368a26a49110072
dharakoya <dharakoya18@gmail.com>
2022-02-28 16:30:02 +0530

    changes

---
+++ CRS-LT-GROUP1-SPRING-JDBCTEMPLATE/src/main/java/com/crs/lt/mapper/GradeMapper.java
@@ -0,0 +1,22 @@
+package com.crs.lt.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.crs.lt.bean.Grade;
+
+public class GradeMapper implements RowMapper<Grade> {
+
+	@Override
+	public Grade mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		Grade grade = new Grade();
+		grade.setCourseCode(rs.getString("courseCode"));
+		grade.setCourseName(rs.getString("courseName"));
+		grade.setGrade(rs.getString("grade"));
+		return grade;
+	}
+
+}
---
+++ CRS-LT-GROUP1-SPRING-JDBCTEMPLATE/src/main/java/com/crs/lt/mapper/ProfessorMapper.java
@@ -0,0 +1,21 @@
+package com.crs.lt.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.crs.lt.bean.Professor;
+
+public class ProfessorMapper implements RowMapper<Professor>
+{
+	@Override
+	public Professor mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		Professor professor = new Professor();
+		professor.setProfessorID(rs.getString("professorID"));
+		professor.setDepartment(rs.getString("department"));
+		professor.setDesignation(rs.getString("designation"));
+		return professor;
+}
+}
